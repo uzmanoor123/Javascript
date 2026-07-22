82bee7f8c81b29ecad15e9e924f073bdb5fade10
uzmanoor123 <uzmanoor200301@gmail.com>
2026-07-22 14:19:59 +0500

    Time: 10 ms (41.54%) | Memory: 66.4 MB (33.85%) - LeetSync

--- 2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.js
+++ 2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.js
@@ -3,9 +3,9 @@
  * @return {number}
  */
 var findNonMinOrMax = function(nums) {
-   for(let i = 0; i<nums.length; i++){
     const max = Math.max(...nums)
     const min = Math.min(...nums)
+   for(let i = 0; i<nums.length; i++){
     if(nums[i]>min && nums[i]< max){
        return nums[i]
     }
