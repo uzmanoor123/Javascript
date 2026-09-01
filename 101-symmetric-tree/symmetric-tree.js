ac7714e44a3994a68449eee7d70b675fbab0861a
uzmanoor123 <uzmanoor200301@gmail.com>
2026-09-01 14:01:35 +0500

    Time: 0 ms (100.00%) | Memory: 58 MB (11.89%) - LeetSync

---
+++ 101-symmetric-tree/symmetric-tree.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    if(root===null){
+        return true
+    }
+    function isMirror(left, right){
+     if(left===null&& right ===null){
+        return true
+    }       
+    if(left===null|| right ===null){
+        return false
+    }
+    if(left.val!= right.val){
+        return false
+    }
+    return isMirror(left.left, right.right)&&
+    isMirror(left.right, right.left)
+    }
+    return isMirror(root.left, root.right)
+};
